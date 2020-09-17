1bb18c39998e53da6ba55cbb259a6aae37b2a168
pokharelshail <shreeshail.pokharel@bison.howard.edu>
2020-09-16 21:01:33 -0400

    API Request and Creating Models

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -3,12 +3,35 @@ package com.codepath.apps.restclienttemplate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import okhttp3.Headers;
 
 public class TimelineActivity extends AppCompatActivity {
-
+    public static final String TAG ="TimelineActivity";
+    TwitterClient client;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
+
+        client = TwitterApplication.getRestClient(this);
+        populateHomeTimeLine();
+    }
+
+    private void populateHomeTimeLine() {
+        client.getHomeTimeLine(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                 Log.i(TAG,"onSucess!" + json.toString());
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.i(TAG,"onFailure!" + response, throwable);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -0,0 +1,27 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class User {
+    public String name;
+    public String screenName;
+    public String imageUrl;
+
+
+    public static User fromJson(JSONObject jsonObject) throws JSONException {
+        User user = new User();
+
+        user.name = jsonObject.getString("name");
+        user.screenName = jsonObject.getString("screen_name");
+        user.imageUrl = jsonObject.getString("profile_image_url_https")
+
+
+        return user;
+
+
+
+
+    }
+
+}
