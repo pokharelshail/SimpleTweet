96bbcd002f8311cb071224953239ce92816289e7
pokharelshail <shreeshail.pokharel@bison.howard.edu>
2020-09-28 22:10:15 -0400

    Compose Tweet

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -0,0 +1,120 @@
+package com.codepath.apps.restclienttemplate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONException;
+import org.parceler.Parcels;
+
+import okhttp3.Headers;
+
+public class ComposeActivity extends AppCompatActivity {
+    public static  final String TAG= "ComposeActivity";
+    public static  final int MAX_TWEET_LENGTH = 280;
+
+    EditText etCompose;
+    Button btnTweet;
+    TwitterClient client;
+    TextView chars;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compose);
+
+        client = TwitterApplication.getRestClient(this);
+        chars = findViewById(R.id.tvChars);
+
+        etCompose = findViewById(R.id.etCompose);
+        etCompose.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                int character  = etCompose.length();
+                character = MAX_TWEET_LENGTH - character;
+                String convert = String.valueOf(character);
+                if (character <0){
+                    chars.setTextColor(Color.RED);
+                    chars.setText("Remaining Characters: "+ convert);
+                }
+                else{
+                    chars.setTextColor(Color.GRAY);
+                    chars.setText("Remaining Characters: "+ convert);
+                }
+
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        btnTweet = findViewById(R.id.btnTweet);
+
+
+        btnTweet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String tweetContent = etCompose.getText().toString();
+                if (tweetContent.isEmpty()){
+                    Toast.makeText(ComposeActivity.this, "Sorry, your tweet can not be empty",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (tweetContent.length() > MAX_TWEET_LENGTH ) {
+                    Toast.makeText(ComposeActivity.this, "Sorry, your tweet is too long",Toast.LENGTH_SHORT).show();
+                    return;
+
+                }
+                Toast.makeText(ComposeActivity.this, tweetContent, Toast.LENGTH_SHORT).show();
+
+                client.publishTweet(tweetContent, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Headers headers, JSON json) {
+                        Log.i(TAG,"onSuccess to publish tweet");
+                        try {
+                            Tweet tweet = Tweet.fromJson(json.jsonObject);
+                            Log.i(TAG,"published tweet says: " + tweet.body);
+                            Intent intent = new Intent();
+                            intent.putExtra("tweet", Parcels.wrap(tweet));
+                            setResult(RESULT_OK,intent);
+                            finish();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                        Log.e(TAG,"onFailure to publish tweet", throwable);
+
+                    }
+                });
+
+
+            }
+        });
+    }
+}
